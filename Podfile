b3dd582b43f88924d059f27d7ec3cd351385b811
kanta-ishii <kn.is.100000000@gmail.com>
2020-11-16 23:53:47 +0900

    No.1 ・カレンダーを作ってみる  ・FSCalendarを使って

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'calendar_sample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for calendar_sample
+
+pod 'FSCalendar' 
+
+end
---
+++ calendar_sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calendar_sample
+//
+//  Created by 石井寛太 on 2020/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
